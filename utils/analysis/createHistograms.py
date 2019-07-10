947badae60ff41546260f98ebb83207e3c90ffcd
Anna Zaborowska <anna.zaborowska@cern.ch>
2019-07-10 16:03:59 +0200

    Add possibility to specify unit of energy

--- utils/analysis/createHistograms.py
+++ utils/analysis/createHistograms.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     parser.add_argument('inputs', type=str, nargs='+', help="name of the input files")
     parser.add_argument('--minEnergy', '-min', dest='minEnergy', type=float, default=90, help="Minimal energy used for binning")
     parser.add_argument('--maxEnergy', '-max', dest='maxEnergy', type=float, default=110, help="Maximal energy used for binning")
+    parser.add_argument('--unitToMeV', '-unit', dest='unit', type=float, default=1, help="Conversion of unit to get MeV for cell energy")
     args = parser.parse_args()
 
     for in_file in args.inputs:
         out_file = "./histograms_" + in_file.split("/")[-1]
         print "input file: ", in_file
         print "output file: ", out_file
-        createHistograms(in_file, out_file, args.minEnergy, args.maxEnergy)
+        createHistograms(in_file, out_file, args.minEnergy, args.maxEnergy, args.unit)
