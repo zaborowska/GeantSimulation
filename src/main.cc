5d4aabcbbbfd41dc0f07249afc6bac21fd1c9afc
Anna Zaborowska <anna.zaborowska@cern.ch>
2020-02-25 10:21:01 +0100

    Use default of 4 threads in MT

--- src/main.cc
+++ src/main.cc
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
   }
 #ifdef G4MULTITHREADED
    G4MTRunManager* runManager = new G4MTRunManager;
-   runManager->SetNumberOfThreads(1);
+   runManager->SetNumberOfThreads(4);
 #else
    G4RunManager* runManager = new G4RunManager;
 #endif
