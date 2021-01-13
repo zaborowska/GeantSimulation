e32361f4b78ccdeaa704a07380a2faf62028ae21
Anna Zaborowska <anna.zaborowska@cern.ch>
2021-01-13 08:55:14 +0100

    Change ranges on control histograms

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -20,21 +20,21 @@ RunAction::RunAction(SaveToFileEventAction* eventAction)
   analysisManager->SetVerboseLevel(1);
   
   // Creating histograms
-  G4double maxEnergy = 50 * GeV;
-  G4double maxLong = 60 * 4.45 * mm;
-  G4double maxTrans = 98 * mm;
-  G4int numLong = 60;
+  G4double maxEnergy = 500 * GeV;
+  G4double maxLong = 500 * mm;
+  G4double maxTrans = 100 * mm;
+  G4int numLong = 200;
   G4int numTrans = 1000;
   analysisManager->CreateH1("EnergyParticle", "Primary energy;E_{MC} (MeV);Normalised entries", 1000, 0, 1.1 * maxEnergy);
   analysisManager->CreateH1("EnergyDep", "Deposited energy;E_{MC} (MeV);Normalised entries", 1000, 0, 1.1 * maxEnergy);
   analysisManager->CreateH1("longProfile", "Longitudinal profile;t (ID);#LTE#GT (MeV)", numLong, 0., numLong);
   analysisManager->CreateH1("transProfile", "Transverse profile;r (ID);#LTE#GT (MeV)", numTrans, 0, numTrans);
-  analysisManager->CreateH1("Time", "Simulation time; time (s);Normalised entries", 3000, 0, 30);
+  analysisManager->CreateH1("Time", "Simulation time; time (s);Normalised entries", 3064, 0, 30);
   analysisManager->CreateH1("EnergyRatio", "Ratio of deposited to primary energy;E_{dep} /  E_{MC};Normalised entries", 100, 0, 1);
-  analysisManager->CreateH1("longFirstMoment", "Longitudinal first moment;#LT#lambda#GT (ID);Normalised entries", 100, 0, maxLong);
-  analysisManager->CreateH1("transFirstMoment", "Transverse first moment;#LTr#GT (ID);Normalised entries", 100, 0, maxTrans/10.);
-  analysisManager->CreateH1("longSecondMoment", "Longitudinal second moment;#LT#lambda^{2}#GT (ID^{2});Normalised entries", 100, 0, pow(maxLong,2)/10);
-  analysisManager->CreateH1("transSecondMoment", "Transverse second moment;#LTr^{2}#GT (ID^{2});Normalised entries", 100, 0, pow(maxTrans/10.,2));
+  analysisManager->CreateH1("longFirstMoment", "Longitudinal first moment;#LT#lambda#GT (ID);Normalised entries", 1024, 0, maxLong);
+  analysisManager->CreateH1("transFirstMoment", "Transverse first moment;#LTr#GT (ID);Normalised entries", 1024, 0, maxTrans/10.);
+  analysisManager->CreateH1("longSecondMoment", "Longitudinal second moment;#LT#lambda^{2}#GT (ID^{2});Normalised entries", 1024, 0, pow(maxLong,2)/200);
+  analysisManager->CreateH1("transSecondMoment", "Transverse second moment;#LTr^{2}#GT (ID^{2});Normalised entries", 1024, 0, pow(maxTrans/10.,2));
 
   // Creating ntuple
   analysisManager->CreateNtuple("events", "per event data");
